00f8fb909d262ad83daa402cd9b6a7e4605d59d2
Qking <2236251136@qq.com>
2020-09-28 15:33:57 +0800

    登录的界面ui做好了

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
 import { Button } from 'element-ui'
+import { Form,FormItem } from 'element-ui'
+import{ input } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(input)
