a2f778b209400ce954a8c7c76e77abf6dafbed5b
Qking <2236251136@qq.com>
2020-09-29 11:40:32 +0800

    输入框的矢量图引入使用，设置输入框的输入规则，表单的重置功能，登录前的表单验证

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,19 +6,21 @@
         <img src="../assets/logo.png" alt="" srcset="" />
       </div>
       <!-- 登录表单区 -->
-      <el-form label-width="0px" class="login_form">
+      <!-- ref获取到表单的引用对象  this.$refs.loginFormRef进行使用 -->
+      <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormrules" label-width="0px" class="login_form">
         <!-- 用户信息 -->
-        <el-form-item>
-          <el-input></el-input>
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
         </el-form-item>
         <!-- 用户密码 -->
-        <el-form-item>
-          <el-input></el-input>
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password" prefix-icon="iconfont icon-3702mima"
+          type="password"></el-input>
         </el-form-item>
         <!-- 按钮 -->
         <el-form-item class="btns">
-          <el-button type="primary">登录</el-button>
-          <el-button type="info">重置</el-button>
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginFrom">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -26,7 +28,41 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      loginForm: {
+        username:'zs',
+        password:'123'
+      },
+      // 登录验证规则
+       loginFormrules: {
+          username: [
+            { required: true, message: '请输入登录名称', trigger: 'blur' },
+            { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+          ],
+           password: [
+            { required: true, message: '请输入用户密码', trigger: 'blur' },
+            { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+          ],
+       }
+    };
+  },
+  methods:{
+    // 重置按钮
+    resetLoginFrom() {
+      // console.log(this)
+      // 拿到this里面的$refs里的定义好的loginFormRef再调用resetFields方法
+      this.$refs.loginFormRef.resetFields()
+    },
+    login(){
+      // 表单验证，validate回调一个参数boolean类型
+      this.$refs.loginFormRef.validate(valid=>{
+        console.log(valid)
+      })
+    }
+  }
+};
 </script>
 
 <style lang="less" scoped>
@@ -63,15 +99,15 @@ export default {};
     }
   }
 }
-.login_form{
-    position: absolute;
-    width: 100%;
-    bottom: 0;
-    padding: 0 20px;
-    box-sizing: border-box;
+.login_form {
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  padding: 0 20px;
+  box-sizing: border-box;
 }
-.btns{
-    display: flex;
-    justify-content: flex-end;
+.btns {
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import router from './router'
 import './plugins/element.js'
 import App from './App.vue'
+import './assets/fonts/iconfont.css'
 import './assets/css/global.css'
 
 Vue.config.productionTip = false
